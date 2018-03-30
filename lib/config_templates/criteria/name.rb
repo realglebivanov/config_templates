97f5285d9e44628326980b084dce4eec958af5cc
Gleb Ivanov <g.ivanov@fun-box.ru>
2018-03-30 13:07:26 +0400

    Fixed name criteria to work properly with all types of expressions

--- lib/config_templates/criteria/name.rb
+++ lib/config_templates/criteria/name.rb
@@ -1,12 +1,20 @@
 module ConfigTemplates::Criteria
   class Name
-    def initialize(pattern, override = true)
+    def initialize(pattern, straight = true)
       @pattern = pattern
-      @override = override
+      @straight = straight
     end
 
-    def matches?(target)
-      @pattern.match?(target) && @override
+    def matches?(expression)
+      @pattern.match?(cast expression) && @straight
+    end
+
+    def cast(expression)
+      if expression.is_a?(::String) || expression.is_a?(::Regexp)
+        expression
+      else
+        expression.to_s
+      end
     end
   end
 end
